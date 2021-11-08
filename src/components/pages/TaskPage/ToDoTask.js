a268cb5d06c1eff21ddcb72407d216cc88201df4
acemark1 <jeyachandranajinath@gmail.com>
2021-11-08 14:09:28 +0000

    Updated Task into Arrays

--- src/components/pages/TaskPage/ToDoTask.js
+++ src/components/pages/TaskPage/ToDoTask.js
@@ -1,25 +1,65 @@
 import React from 'react'
 import TaskBox from './TaskBox';
 
+
+const todoTask = [
+    <TaskBox 
+    projectName="Nodlehs"
+    projectDetail="We need a process that will track the inventory and status of components in the chassis.
+              It should accept IPC requests from the various cards, maintain an internal database of what's currently available,
+              respond to IPC queries from other components and have a CLI interface so that we can query its contents. We will be
+              adding capabilities above and beyond this in the future."
+    projectDue="Tomorrow"
+    projectPriority="1"
+    projectChekced="false"
+    projectLoggedInIcon="./images/sheldon.jpg"
+    projectLoggedInUsername="sheldcooper93"
+    ProjectTag=""    // Make this array
+    />,
+    <TaskBox 
+    projectName="Nodlehs"
+    projectDetail="We need a process that will track the inventory and status of components in the chassis.
+              It should accept IPC requests from the various cards, maintain an internal database of what's currently available,
+              respond to IPC queries from other components and have a CLI interface so that we can query its contents. We will be
+              adding capabilities above and beyond this in the future."
+    projectDue="Tomorrow"
+    projectPriority="1"
+    projectChekced="false"
+    projectLoggedInIcon="./images/sheldon.jpg"
+    projectLoggedInUsername="sheldcooper93"
+    ProjectTag=""    // Make this array
+    />,
+    <TaskBox 
+    projectName="Nodlehs"
+    projectDetail="We need a process that will track the inventory and status of components in the chassis.
+              It should accept IPC requests from the various cards, maintain an internal database of what's currently available,
+              respond to IPC queries from other components and have a CLI interface so that we can query its contents. We will be
+              adding capabilities above and beyond this in the future."
+    projectDue="Tomorrow"
+    projectPriority="1"
+    projectChekced="false"
+    projectLoggedInIcon="./images/sheldon.jpg"
+    projectLoggedInUsername="sheldcooper93"
+    ProjectTag=""    // Make this array
+    />,
+
+
+]
 function ToDoTask() {
     return (
         <div>
-                                <TaskBox 
-            projectName="Nodlehs"
-            projectDetail="We need a process that will track the inventory and status of components in the chassis.
-                      It should accept IPC requests from the various cards, maintain an internal database of what's currently available,
-                      respond to IPC queries from other components and have a CLI interface so that we can query its contents. We will be
-                      adding capabilities above and beyond this in the future."
-            projectDue="Tomorrow"
-            projectPriority="1"
-            projectChekced="false"
-            projectLoggedInIcon="./images/sheldon.jpg"
-            projectLoggedInUsername="sheldcooper93"
-            ProjectTag=""    // Make this array
-            />
+        {/* Get data from API  */}
+        {todoTask.map(todo => todo)}
+        
+
+
+                               
         </div>
     )
 }
+
+
+
 
 export default ToDoTask;
 
